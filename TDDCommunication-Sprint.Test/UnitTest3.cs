1d81e3d1fb083c44dafed7c72ea1251ee029ede3
jodielmclaughlin <jodieleighmclaughlin@gmail.com>
2026-03-24 15:04:44 +0000

    feat: finished Kata 3 - Add up the ASCII codes

---
+++ TDDCommunication-Sprint.Test/UnitTest3.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.VisualStudio.TestPlatform.Utilities;
+using Shouldly;
+
+namespace TDDCommunication_Sprint.Test
+{
+    public class UnitTest3
+    {
+        [Test]
+        public void SumUpASCII_ShouldReturnSumOf1Character()
+        {
+            int output = ASCIICodes.SumUpAscii("a");
+            output.ShouldBe(97);
+        }
+
+        [Test]
+        public void SumUpASCII_ShouldReturnSumOf2Characters()
+        {
+            int output = ASCIICodes.SumUpAscii("aA");
+            output.ShouldBe(162);
+        }
+
+        [Test]
+        public void SumUpASCII_ShouldReturnSumOf11Characters()
+        {
+            int output = ASCIICodes.SumUpAscii("Northcoders");
+            output.ShouldBe(1163);
+        }
+        [Test]
+        public void SumUpASCII_ShouldReturnSumOf5Numbers()
+        {
+            int output = ASCIICodes.SumUpAscii("98765");
+            output.ShouldBe(275);
+        }
+    }
+}
---
+++ TDDCommunication-Sprint/ASCIICodes.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TDDCommunication_Sprint
+{
+    public class ASCIICodes
+    {
+        public static int SumUpAscii(string ascii)
+        {
+            char[] splitString = ascii.ToCharArray();
+            
+            int convertedAscii = 0;
+
+            foreach (char c in splitString)
+            {
+                convertedAscii += Convert.ToInt32(c);
+            }
+
+            return convertedAscii;
+         }
+    }
+}
