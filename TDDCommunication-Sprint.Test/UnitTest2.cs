c0830f32fb9903874bf6204e85c957f240e8bdc0
Ness <sampaio_88@hotmail.com>
2026-03-24 14:26:47 +0000

    Added Empty Array Test

---
+++ TDDCommunication-Sprint.Test/UnitTest2.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestPlatform.Utilities;
+using Shouldly;
+
+namespace TDDCommunication_Sprint.Test
+{
+    public class UnitTest2
+    {
+        [Test]
+        public void CaculateMeanValue_OfAnEmptyArray()
+        {
+            double output = MeanNumbers.CalculateMeanValue([]);
+            output.ShouldBe(0);
+        }
+            [Test]
+        public void CaculateMeanValue_OfaOneNumberArray()
+        {
+            double output = MeanNumbers.CalculateMeanValue([3]);
+            output.ShouldBe(3);
+        }
+
+        [Test]
+        public void CalculateMeanValue_OfAMultipleNumberArray()
+        {
+            double output = MeanNumbers.CalculateMeanValue([4, 2, 6, 3, 6]);
+            output.ShouldBe(4.2);
+        }
+    }
+}
---
+++ TDDCommunication-Sprint/MeanNumbers.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TDDCommunication_Sprint
+{
+    public class MeanNumbers
+    {
+        public static double CalculateMeanValue(int[] nums)
+        {
+            if (nums.Length == 0)
+            {
+                return 0;
+            }
+            if (nums.Length == 1)
+            {
+
+                return nums[0];
+            }
+            double sumArrayNums = 0;
+            if (nums.Length >= 2)
+            {
+
+            for (int i = 0; i <nums.Length; i++)
+                {
+                    sumArrayNums += nums[i];
+                 }
+             }
+            return sumArrayNums / nums.Length;
+        }
+     }
+}
