a9f319a1656826e6427644d5b3fdd6cba0ea4f4d
jodielmclaughlin <jodieleighmclaughlin@gmail.com>
2026-03-24 17:04:09 +0000

    Started on Kata 5  A Caesar Cipher

---
+++ TDDCommunication-Sprint.Test/UnitTest5.cs
@@ -0,0 +1,14 @@
+﻿using Shouldly;
+
+namespace TDDCommunication_Sprint.Test
+{
+    public class UnitTest5
+    {
+        [Test]
+        public void SwappedCipher_ShouldReturnNForA()
+        {
+            string output = CaeserCipher.SwappedCipher("AAA", 13);
+            output.ShouldBe("NNN");
+        }
+    }
+}
---
+++ TDDCommunication-Sprint/CaeserCipher.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TDDCommunication_Sprint
+{
+    public class CaeserCipher
+    {
+        public static string SwappedCipher(string cipher, int num)
+        {
+            string alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz";
+            string result = String.Empty;
+
+            foreach (char letter in cipher)
+            {
+                char nextLetter = letter;
+                
+                if ((letter >= 'A' && letter <= 'Z') || (letter >= 'a' && letter <= 'z'))
+                {
+                    nextLetter = alphabet[(alphabet.IndexOf(nextLetter) + num)];
+                }
+                result += nextLetter;
+            }
+
+            return result;
+        }
+    }
+}
